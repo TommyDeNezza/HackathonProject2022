c873d8e7010ffcbe0d6f0507c8c10383e5c3e0a6
max-montes <77820353+max-montes@users.noreply.github.com>
2022-11-12 17:18:13 -0500

    Selenium file added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # HackathonProject2022
 
 ## Goals:
-    Create Python script that automatically enrolls you into courses
+Create Python script that automatically enrolls you into courses
 
 ## setup
 pip install selenium (run this in top level folder of project)
--- seleniumScript.py
+++ seleniumScript.py
@@ -1,4 +1,15 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
-r = requests.post('https://httpbin.org/post', data={'key': 'value'})
-
+driver = webdriver.Chrome()
+username = "montessm"
+password = "Shak1r81"
+# Agora link
+driver.get("https://login.bc.edu/nidp/idff/sso?id=19&sid=0&option=credential&sid=0&target=https%3A%2F%2Fservices.bc.edu%2Fcommoncore%2Fmyservices.do")
+# registration page link
+driver.get("https://eaen.bc.edu/student-registration/#/")
+elem = driver.find_element(By.XPATH, '//*[@id="tabularCCRegistrationRequestItemSelectorCheckbox2"]')
+elem.click()
+elem = driver.find_element(By.XPATH, '//*[@id="register-button"]')
